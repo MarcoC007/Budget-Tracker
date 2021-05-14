2119a2fe9b8f0552544f910afdafa98ff207dab3
Marco Estrada <marco2110estrada@gmail.com>
2021-05-13 20:40:51 -0400

    adding service-worker file

---
+++ public/service-worker.js
@@ -0,0 +1,89 @@
+const FILES_TO_CACHE = [
+'/',
+'/index.js',
+'/index.html',
+'/db.js',
+'/manifest.webmanifest',
+'/styles.css',
+'/icons/icon-192x192.png',
+'/icons/icon-512-512.png'
+
+];
+
+const STATIC_CACHE = 'static-cache-v1';
+const RUNTIME_CACHE = 'runtime-cache-v1';
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches
+        .open(STATIC_CACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+});
+
+// The activate handler takes care of cleaning up the cache
+
+self.addEventListener('activate', e => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    e.waitUntil(
+        caches
+        .keys()
+        .then(cacheNames => {
+            return cacheNames.filter(
+                cacheName => !currentCaches.includes(cacheName)
+            );
+        })
+        .then(cachesToDelete => {
+            return Promise.all(
+                cachesToDelete.map(cacheToDelete => {
+                    return caches.delete(cacheToDelete);
+                })
+            );
+        })
+        .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', e => {
+    if(
+       e.request.method !== 'GET' ||
+       !e.request.url.startsWith(self.location.origin)
+    ) {
+        e.respondWith(fetch(e.request));
+        return;
+    }
+
+    // handle runtime GET requests for data from /api routes
+    if (e.request.url.includes('/api')) {
+        e.respondWith(
+            caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(e.response)
+                .then(response => {
+                    cache.put(e.request, response.clone());
+                    return response;
+                })
+                .catch(() => caches.match(e.request));
+            })
+        );
+        return;
+    }
+
+    // use cache first for all other requests for performance
+    e.respondWith(
+        caches.match(e.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            // request is not in cache. make network request and cache the response 
+            return caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(e.request).then(response => {
+                    return cache.put(e.request, response.clone()).then(() => {
+                        return response;
+                    });
+                });
+            });
+        })
+    );
+});
